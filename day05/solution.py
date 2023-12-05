b4e237a22d48f2cf6d89038b5b4f15e617a8aee1
Rene Trampenau <rtrampenau@gmail.com>
2023-12-05 20:13:32 +0100

    added day05 part1

---
+++ day05/solution.py
@@ -0,0 +1,44 @@
+import os
+
+def extract_data(data):
+    maps = {}
+    seeds = [int(x.strip()) for x in data[0].split(':')[-1].split()]
+
+    for i, line in enumerate(data):
+        if i == 0 or line == "":
+            continue
+
+        if ":" in line:
+            current = line[:-1]
+            continue
+        else:
+            if current not in maps:
+                maps[current] = [[int(x.strip()) for x in line.split()]]
+            else:
+                maps[current].append([int(x.strip()) for x in line.split()])
+    return seeds, maps
+
+def part1(data):
+    seeds, maps = extract_data(data)
+    for _, map in maps.items():
+        for i, seed in enumerate(seeds):
+            for rule in map:
+                dest  = rule[0]
+                src   = rule[1]
+                range = rule[2]
+                if src <= seed < (src + range):
+                    seeds[i] = dest + (seed - src)
+
+    return min(seeds)   
+
+
+def part2(data):
+    seeds, maps = extract_data(data)
+    pass
+
+if __name__ == "__main__":
+    with open(os.path.dirname(os.path.realpath(__file__)) + '\\data.txt') as f:
+        data = f.read().splitlines()
+
+    print(part1(data))
+    print(part2(data))
